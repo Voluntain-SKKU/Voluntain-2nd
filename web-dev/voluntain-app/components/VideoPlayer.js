4018b975612dc9e70de810e8b4209e976b29258a
SG-SKKU <laino1227@gmail.com>
2021-07-22 20:42:34 +0900

    Add feature to resize video
    VideoPlayer.js now uses React Hooks instead of
    ES6 classes.
    useWindowSize.js is created to get the current browser size.
    Using this hook, VideoPlayer get width and height of current browser, and resizes player if they are too small
    to display 720*480 videos.
    The overall usage is the same as before.

--- web-dev/voluntain-app/components/VideoPlayer.js
+++ web-dev/voluntain-app/components/VideoPlayer.js
@@ -1,35 +1,32 @@
 import React from 'react'
 import Youtube from 'react-youtube'
-
-/**
- * @Usage
- *     \<VideoPlayer videoId='_9RvpFdUQr0' \/\>
- */
-export const VideoPlayer = (props) => {
-    return (
-        <YoutubePlayer videoId={props.videoId}/>
-    );
-}
+import { useWindowSize } from './useWindowSize';
 
 /**
  * @see
  * https://developers.google.com/youtube/iframe_api_reference
+ * https://developers.google.com/youtube/player_parameters
+ * 
+ * @Usage
+ *     \<VideoPlayer videoId='_9RvpFdUQr0' \/\>
  */
-class YoutubePlayer extends React.Component {
-    render() {
-      const opts = {
-        height: '480',
-        width: '720',
-        playerVars: {
-          // To check other variables, check:
-          // https://developers.google.com/youtube/player_parameters
-          cc_load_policy: 1,
-          modestbranding: 1,
-        }
+export const VideoPlayer = (props) => {
+    const size = useWindowSize();
+
+    const opts = {
+      height: size.height > 480? '480' : size.height,
+      width: size.width > 720 ? '720' : size.width,
+      playerVars: {
+        // To check other variables, check:
+        // https://developers.google.com/youtube/player_parameters
+        cc_load_policy: 1,
+        modestbranding: 1,
       }
-  
-      const { videoId } = this.props
-  
-      return <Youtube videoId={videoId} opts={opts} />;
     }
-  }
+
+    const { videoId } = props
+
+    return (
+      <Youtube videoId={videoId} opts={opts} />
+    );
+}
