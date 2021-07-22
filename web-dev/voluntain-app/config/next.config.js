76999e2898ad6e526321f14a04670b8c7faad76a
hwiwonK <gnldnjs2469@gmail.com>
2021-07-23 00:40:38 +0900

    _

--- web-dev/voluntain-app/config/next.config.js
+++ web-dev/voluntain-app/config/next.config.js
@@ -2,4 +2,4 @@ const production = process.env.NODE_ENV === "production";
 
 export const url = production
   ? "https://www.yoursite.com"
-  : "https://localhost:1337";
+  : "http://localhost:1337";
