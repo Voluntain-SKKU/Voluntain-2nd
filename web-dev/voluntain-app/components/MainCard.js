a5303df75a7896a18d5dd1f8db8dec222476aad7
Sanhaa <livia3609@gmail.com>
2021-07-27 20:15:08 +0900

    Modify main card layout

--- web-dev/voluntain-app/components/MainCard.js
+++ web-dev/voluntain-app/components/MainCard.js
@@ -1,21 +1,22 @@
 import styles from '../styles/Home.module.css'
 import { CourseCard } from './CourseCard';
 import CardColumns from 'react-bootstrap/CardColumns'
+import { CardGroup } from 'react-bootstrap';
 
 export const MainCard = (props)=> {
 
     return(
-        <CardColumns>
+        <CardGroup className="d-flex">
             {/* get course inform from backend */}
-        {props.courses.map((course) => (
-            <CourseCard
-            title={course.title} 
-            img={"/"+course.title+".png"}
-            content={course.about}
-            link={"/" + course.id}
-            level={course.level}
-            />  
-          ))}
-        </CardColumns>
+            {props.courses.map((course) => (
+                <CourseCard
+                title={course.title} 
+                img={"/"+course.title+".png"}
+                content={course.about}
+                link={"/" + course.id}
+                level={course.level}
+                />  
+            ))}
+        </CardGroup>
     );
 }
