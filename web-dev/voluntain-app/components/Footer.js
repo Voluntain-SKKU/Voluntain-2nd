0bfa2a4efd712f1347eab16de26a8eb7295db9b4
SG-SKKU <laino1227@gmail.com>
2021-08-05 21:25:14 +0900

    Add footer
    Now footer has buttons to email, slack, youtube.
    Copyright message has been added.
    Related contents have been added to CSS file to construct the footer.

--- web-dev/voluntain-app/components/Footer.js
+++ web-dev/voluntain-app/components/Footer.js
@@ -1,25 +1,54 @@
 import React from 'react'
-import { Grid, Link } from '@material-ui/core';
+import { Grid, Popover } from '@material-ui/core';
+import { Forum, MailOutline, YouTube } from '@material-ui/icons';
 
 import styles from '../styles/Home.module.css'
 
 export const Footer = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleEmail = (event) => {
+    setAnchorEl(event.currentTarget);
+    copyEmail();
+  }
+  const handleEmailClose = () => {
+    setAnchorEl(null);
+  }
+  const copyEmail = () => {
+    const email = document.createElement('textarea');
+    document.body.appendChild(email);
+    email.value = 'skku.voluntain@gmail.com'
+    email.select();
+    document.execCommand('copy');
+    document.body.removeChild(email);
+  }
+  const openEmail = Boolean(anchorEl);
+
   return (
     <footer className={styles.footer}>
-      <Grid container direction='row' justifyContent='center' alignItems='flex-start' spacing={2}>
+      <Grid container direction='row' justifyContent='center' alignItems='center' spacing={2}>
         <Grid item className={styles.footerItem}>
-          <strong>EMAIL</strong> {' '}
-          skku.voluntain@gmail.com
+          <a href='#!' onClick={handleEmail}><MailOutline />{' '}Email Us</a>
+          <Popover
+            open={openEmail}
+            anchorEl={anchorEl}
+            onClose={handleEmailClose}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+          >
+            <div style={{padding: 5}}>Copied to clipboard: skku.voluntain@gmail.com</div>
+          </Popover>
         </Grid>
         <Grid item className={styles.footerItem}>
-          <strong>CONTACT</strong> {' '}
-          +82-00-0000-0000
-        </Grid>
-        <Grid item className={styles.footerItem}>
-          <strong><a href='./setting'>Cookie Policy</a></strong>
+          <a href='./setting'><Forum />{' '}Slack</a>
         </Grid>
         <Grid item>
-          <strong><a href='https://www.youtube.com/channel/UCYvzm_6k-_V4UHFjEMdFtuA'>Youtube Channel</a></strong>
+          <a href='https://www.youtube.com/channel/UCYvzm_6k-_V4UHFjEMdFtuA'><YouTube />{' '}Youtube</a>
         </Grid>
       </Grid>
       <div className={styles.copyright}>© 2021 Voluntain. All rights reserved.</div>
