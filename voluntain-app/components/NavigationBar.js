ffaf044da7e2cde6d4354874998a0eeae39be8ab
Sanhaa <livia3609@gmail.com>
2021-08-12 00:58:05 +0900

    file, directory 정리

---
+++ voluntain-app/components/NavigationBar.js
@@ -0,0 +1,43 @@
+/*  ./components/Navbar.js     */
+import Link from 'next/link';
+import Image from 'next/image';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import styles from '../styles/Home.module.css'
+
+// export default - const difference
+export const NavigationBar = (props) => {
+  
+  return (
+    <div className={styles.navwrapper}>
+      <Navbar bg="light" expand="md" className={styles.navbar}>
+        <Container className={styles.navcontainer}>
+          <Navbar.Brand href="/">
+          <img
+            src="/favicon.ico"
+            width="35"
+            height="35"
+            className="d-inline-block align-top"
+            alt="Voluntain logo"
+          />&nbsp;&nbsp;
+           <span className={styles.titlefont}>VOLUNTAIN</span></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav"> 
+            <Nav className="ms-auto">
+              <Nav.Link href="/">HOME</Nav.Link>
+              <Nav.Link href="/about">ABOUT</Nav.Link>
+              <Nav.Link href="/question">Q&A</Nav.Link>
+              <NavDropdown title="COURSES" id="basic-nav-dropdown">
+                {props.titles.map((item) => (
+                  <NavDropdown.Item href={"/course/" + item.id}>{item.title}</NavDropdown.Item>
+                ))}
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Other</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
---
+++ voluntain-app/pages/_app.js
@@ -0,0 +1,46 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { CookiesProvider, useCookies } from 'react-cookie'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from '../lib/ga'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const [cookies, setCookie, removeCookie] = useCookies(['noAnalytics']);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+      window['ga-disable-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'] = ${cookies.noAnalytics};
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+      `
+        }} />
+      </Head>
+      <CookiesProvider><Component {...pageProps} /></CookiesProvider>
+    </>
+  );
+}
+
+export default MyApp;
