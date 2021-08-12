8cce8d2b58a44db4a3bf6ab5cc2968bbc65e66b7
SG-SKKU <laino1227@gmail.com>
2021-08-12 23:17:52 +0900

    FIx the feature to recommend the next lecture
    The link to the next lecture is replaced by a button,
    to handle the event that increments the lectureId cookie value.
    Mapping the lecture name and id is now handled in MainCookieCard.js
    instead of RecentLecture.js

--- voluntain-app/components/MainCookieCard.js
+++ voluntain-app/components/MainCookieCard.js
@@ -1,25 +1,58 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useCookies } from 'react-cookie'
 import { Collapse } from 'react-bootstrap'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
 import styles from '../styles/Home.module.css'
-import { Alert, AlertTitle } from '@material-ui/lab';
+import { Alert } from '@material-ui/lab';
+import { Button } from '@material-ui/core';
 
-export function MainCookieCard( props ) {
+export function MainCookieCard(props) {
+  const [cookies, setCookie, removeCookie] = useCookies(['lectureId', 'videoEnd', 'isLastLecture']);
+  const [lectureTitle, setLectureTitle] = useState("");
+
+  /**
+   * Set lecture title only if there is history
+   */
+  React.useEffect(() => {
+    if (cookies.lectureId !== undefined) {
+      console.log(props.lectures);
+      Object.keys(props.lectures).forEach(function (key) {
+        console.log("Getting lecture #", props.lectures[key]['lecture_number']);
+        if ((cookies.videoEnd != 1) && (props.lectures[key]['lecture_number'] == (cookies.lectureId))) {
+          // when user has not finished the recent lecture
+          console.log(`MATCH: the current lecture ${cookies.lectureId}`);
+          setLectureTitle(props.lectures[key]['title']);
+        } else if ((cookies.videoEnd == 1) && (cookies.isLastLecture != 1) && (props.lectures[key]['lecture_number'] == (cookies.lectureId + 1))) {
+          // when user has finished the recent lecture, which is not the last one of the course.
+          console.log(`MATCH: the next lecture ${cookies.lectureId + 1}`);
+          setLectureTitle(props.lectures[key]['title']);
+        }
+      })
+    }
+  }, []);
+
   const [open, setOpen] = useState(true);
-  function handleClick(){
+  function handleClose() {
     props.handleClose();
     setOpen(false);
   }
+
+  const handleLinkClick = () => {
+    let nextId = parseInt(cookies.lectureId) + 1
+    if (cookies.lectureId !== undefined && cookies.videoEnd == 1 && cookies.isLastLecture != 1) {
+      console.log(`setting cookie to the next lecture: ` + nextId);
+      setCookie('lectureId', nextId, { path: '/', maxAge: 31536000 });
+      setCookie('videoEnd', 0, { path: '/', maxAge: 31536000 });
+    }
+  }
+
   return (
     <Collapse in={open}>
-      <Alert onClose={() => {handleClick()}} severity={props.severity} className={styles.cookiecard}>
-          <span className={styles.text}>
+      <Alert onClose={() => { handleClose() }} severity={props.severity} className={styles.cookiecard}>
+        <span className={styles.text}>
           {props.title}
           &nbsp;&nbsp;
-          <a href={props.link}>{props.lectureTitle?props.lectureTitle:props.text}</a>
-          </span>     
+          <Button href={props.link} color='primary' onClick={handleLinkClick}>{lectureTitle ? lectureTitle : props.text}</Button>
+        </span>
       </Alert>
     </Collapse>
   );
