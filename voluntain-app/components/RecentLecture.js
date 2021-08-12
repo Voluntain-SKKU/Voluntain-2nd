e3a1c367c88369e47366d5cc67f9d2213c5a0649
SG-SKKU <laino1227@gmail.com>
2021-08-12 23:13:57 +0900

    FIx the feature to recommend the next lecture
    The link to the next lecture is replaced by a button,
    to handle the event that increments the lectureId cookie value.
    Mapping the lecture name and id is now handled in MainCookieCard.js
    instead of RecentLecture.js

--- voluntain-app/components/RecentLecture.js
+++ voluntain-app/components/RecentLecture.js
@@ -1,47 +1,24 @@
-/**
- * Obsolete,
- * merged to VideoStateChecker.
- */
 import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { useCookies } from 'react-cookie'
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { MainCookieCard } from './MainCookieCard';
 
-
-// 해당 course에서 특정 lecture 찾기
-// const findLecture = (lectures) =>{
-//     console.log('findlecture', lectures);
-//     for(var lec in lectures){
-//         console.log('sadfasdf', lec);
-//         if(lec['lecture_num'] == cookies.lectureId+1){
-//             console.log('title: ', lec['title'])
-//             return lec['title'];
-//         }
-//     }
-//     return null;
-// }
-
-/**
- * VideoStateChecker 로직 수정
- */
-export const RecentLecture = ( props ) => {
+export const RecentLecture = (props) => {
     const [title, setTitle] = useState("Select the course below.");
     const [text, setText] = useState("");
     const [link, setLink] = useState("");
     const [contenttitle, setContenttitle] = useState("Continue Learning");
     const [severity, setSeverity] = useState("warning");
-    
-    const [lectureTitle, setLectureTitle] = useState("");
-    
+
     const [cookies, setCookie, removeCookie] = useCookies(['courseId', 'lectureId', 'videoEnd', 'noCookie', 'isLastLecture']);
     const handleClose = () => {
         removeCookie('courseId');
         removeCookie('lectureId');
         removeCookie('videoEnd');
+        removeCookie('isLastLecture');
     };
-    let nextId = cookies.lectureId + 1
 
     useEffect(() => {
         if (cookies.noCookie !== undefined) {
@@ -52,33 +29,26 @@ export const RecentLecture = ( props ) => {
         else if (cookies.lectureId !== undefined) {
             if (cookies.videoEnd == 1) {
                 setSeverity("success")
-                if(cookies.isLastLecture == 1){ 
+                if (cookies.isLastLecture == 1) {
                     // 마지막 강의를 끝까지 다 들었을 때
                     setTitle("Congratulations");
                     setText("You finished a course!");
                     // 다른 코스 추천 안함
                 }
-                else{
-                // 마지막 강의가 아닐 때
+                else {
+                    // 마지막 강의가 아닐 때
                     setTitle("Recommend you to watch");
                     setLink("/course/" + cookies.courseId);
                 }
             }
-            else{
+            else {
                 setTitle("You are watching");
                 setLink("/course/" + cookies.courseId);
                 setSeverity("info");
             }
         }
-        if(cookies.videoEnd != 1 || cookies.isLastLecture != 1){
-            Object.keys(props.lectures).forEach(function(key){
-                console.log("****", props.lectures[key]['lecture_number']);
-                if(props.lectures[key]['lecture_number'] == (cookies.lectureId) ){
-                    setLectureTitle(props.lectures[key]['title']);
-                }
-            })
-        }
-        if(cookies.lectureId == undefined) setContenttitle("Enjoy your learning!")
+
+        if (cookies.lectureId == undefined) setContenttitle("Enjoy your learning!")
     }, []);
     // cookie 값이 없으면 아예 나타내지 않음
     // if 문을 사용하면 레이아웃이 깨지는 듯 함
@@ -86,14 +56,13 @@ export const RecentLecture = ( props ) => {
         <div className={styles.content}>
             <span className={styles.contenttitle}>{contenttitle}</span>
             <MainCookieCard
+                lectures={props.lectures}
                 severity={severity}
                 title={title}
                 text={text}
                 link={link}
-                lectureTitle={lectureTitle}
                 handleClose={handleClose}
             />
         </div>
     );
 };
-  
