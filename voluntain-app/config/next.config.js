4c44fdd585b7caeab4680fe69a94d77603b68b71
Sanhaa <livia3609@gmail.com>
2021-08-24 18:22:20 +0900

    production `

--- voluntain-app/config/next.config.js
+++ voluntain-app/config/next.config.js
@@ -1,10 +1,3 @@
 const production = process.env.NODE_ENV === "production";
 
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  // assetPrefix: production ? 'https://cdn.mydomain.com' : '',
-  assetPrefix: production? "." : "", // css, js 파일 경로를 relative로 설정하기 위해 prefix 필요함
-  // url: production?"http://3.36.123.115:1337": "http://localhost:1337",
-  url: "http://3.36.123.115:1337",
-  trailingSlash: true, // export 한 파일에서 .html 확장자 붙여주기 위함
-}
+export const url= "http://3.36.123.115:1337";
--- voluntain-app/next.config.js
+++ voluntain-app/next.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true, // export 한 파일에서 .html 확장자 붙여주기 위함
+  assetPrefix: 'http://voluntain.s3-website.ap-northeast-2.amazonaws.com' , // css, js 파일 경로를 relative로 설정하기 위해 prefix 필요함
 }
